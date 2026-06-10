e6e8cf1197718a97113bd0dcb92b61246a37eb20
Atharv patil <atharvyp12@gmail.com>
2026-06-10 15:11:27 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (62.77%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,34 +1,34 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        // int n= nums.length;
-        // int left=0;
-        // int right=n-1;
-        // int ans[]=new int[n];
-        // int idex=n-1;
+        int n= nums.length;
+        int left=0;
+        int right=n-1;
+        int ans[]=new int[n];
+        int idex=n-1;
 
-        // while(left <=right){
-        // int leftsq =nums[left]*nums[left];
-        // int rightsq=nums[right]*nums[right];
+        while(left <=right){
+        int leftsq =nums[left]*nums[left];
+        int rightsq=nums[right]*nums[right];
 
 
-        //     if(leftsq<rightsq){
-        //         ans[idex]=rightsq;
-        //         right--;
+            if(leftsq<rightsq){
+                ans[idex]=rightsq;
+                right--;
                 
-        //     }else{
-        //         ans[idex]= leftsq;
-        //         left++;
+            }else{
+                ans[idex]= leftsq;
+                left++;
                 
-        //     }
-        //     idex--;
-        // }
-        // return ans;
-        for(int i =0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-
+            }
+            idex--;
         }
-        Arrays.sort(nums);
-        return nums;
+        return ans;
+        // for(int i =0;i<nums.length;i++){
+        //     nums[i]=nums[i]*nums[i];
+
+        // }
+        // Arrays.sort(nums);
+        // return nums;
 
         
     }
